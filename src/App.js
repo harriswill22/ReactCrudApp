eb0817d1f3a296f8a341d89f2eaba430bea3274d
Will Harris <Harriswill22@yahoo.com>
2021-01-15 11:58:03 -0500

    Notes List name changed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <nav className="navbar bg-light navbar-expand-lg navbar-light">
       <ul className="navbar-nav mr-auto">
         <li className="navbar-item">
-          <Link to="/" className="nav-link">Notes</Link>
+          <Link to="/" className="nav-link">Notes List</Link>
         </li>
         <li className="navbar-item">
           <Link to="/create" className="nav-link">Create Note</Link>
